c87aa35960782114ebd314a5b6a1b147f8df7c64
Jonathan Canales <joncanales2@gmail.com>
2020-09-23 15:55:59 -0400

    Working through initial prompt to generate user input.

--- index.js
+++ index.js
@@ -4,9 +4,26 @@ const inquirer = require('inquirer');
 
 
 // array of questions for user
-const questions = [
+// const questions = [
+// ];
 
-];
+inquirer.prompt([
+    {type: "input",
+     name: "title",
+     message: "What is the title of your project?"},
+    {type: "input",
+     name: "Description",
+     message: "Please provide a description of your project"},
+    {type: "input",
+     name: "Table of Contents",
+     message: "Please provide your table of contents separated by commas"},
+    {type: "input",
+     name: "Installation Instructions",
+     message: "Please provide installation instructions in order separated by commas"},
+    {type: "input",
+     name: "Usage",
+     message: "Please provide usage criteria for your project"},
+])
 
 // function to write README file
 function writeToFile(fileName, data) {
